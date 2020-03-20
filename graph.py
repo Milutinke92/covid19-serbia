faaf4fc5bacfb136af91c4da7cd831faf801f68e
Stefan Milutinovic <stefan.milutinovic@sbgenomics.com>
2020-03-20 18:40:36 +0100

    Included death cases

--- graph.py
+++ graph.py
@@ -6,10 +6,12 @@ import pandas as pd
 df = pd.read_json("data.json")
 df_new_cases = pd.read_json("data_new_cases.json")
 df_cured_cases = pd.read_json("data_cure_cases.json")
+df_death_cases = pd.read_json("data_death_cases.json")
 
 df['time'] = df['time'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
 df_new_cases['time'] = df_new_cases['time'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
 df_cured_cases['time'] = df_cured_cases['time'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+df_death_cases['time'] = df_death_cases['time'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
 
 width = 0.35  # the width of the bars: can also be len(x) sequence
 
@@ -18,6 +20,7 @@ fig, ax = plt.subplots()
 ax.bar(df['time'].array, df['num_of_cases'].array, width, label='all cases')
 ax.bar(df_new_cases['time'].array, df_new_cases['num_of_cases'].array, width, label='new cases')
 ax.bar(df_cured_cases['time'].array, df_cured_cases['num_of_cases'].array, width, label='cured cases')
+ax.bar(df_death_cases['time'].array, df_death_cases['num_of_cases'].array, width, label='death cases')
 
 ax.xaxis_date()
 ax.set_ylabel('Num Of Cases')
